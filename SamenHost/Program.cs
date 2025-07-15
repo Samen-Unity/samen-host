bba5ea358dd2c5be642eba2ff42a4393d6b15950
Richard de Vries <Pruyque@outlook.com>
2025-07-16 00:31:28 +0200

    Stuff

--- SamenHost/Program.cs
+++ SamenHost/Program.cs
@@ -33,34 +33,9 @@ namespace SamenHost
 
             Logging.Log("Startup", $"Loading Network...", LogType.INFO);
             Server.Start(4041);
+            ServerCLI.Begin();
 
             Logging.Log("Samen", $"Startup completed in {Math.Ceiling(DateTime.Now.Subtract(loadStartTime).TotalMilliseconds)}ms!", LogType.IMPORTANT);
-
-            Console.CancelKeyPress += (sender, e) =>
-            {
-                Console.Clear();
-                Console.WriteLine("!!! Stopping Samen will remove all session data !!!");
-                Console.WriteLine("!!! MAKE SURE YOU HAVE YOUR SCENES EXPORTED !!!");
-
-                Console.WriteLine("");
-                Console.WriteLine("Are you sure? Y/N (N)");
-                // Use Quit command
-                var key = Console.ReadKey();
-                if (key.Key != ConsoleKey.Y)
-                {
-                    e.Cancel = true;
-                    Console.Clear();
-                    Logging.Log("CLOSE", "Prevented shutdown", LogType.IMPORTANT);
-                }
-                else
-                {
-                    Console.Clear();
-                    Logging.Log("CLOSE", "Goodbye!", LogType.IMPORTANT);
-                    Environment.Exit(-1);
-                }
-            };
-
-            ServerCLI.Begin();
 
             while (true)
             {
