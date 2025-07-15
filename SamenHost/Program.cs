a432bfca138e3025ddcdf329d25e71448169141e
Pruyque <Pruyque@gmail.com>
2025-07-16 00:27:16 +0200

    Update Program.cs to use ServerCLI

--- SamenHost/Program.cs
+++ SamenHost/Program.cs
@@ -1,8 +1,9 @@
-﻿using Microsoft.Win32;
+using Microsoft.Win32;
 using SamenHost.Chat;
 using SamenHost.Core;
 using SamenHost.Internet;
 using SamenHost.Plugins;
+using SamenHost.ServerCommands;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,7 +14,6 @@ namespace SamenHost
 {
     internal class Program
     {
-
         public static int SamenVersion = 100;
         static void Main(string[] args)
         {
@@ -33,32 +33,9 @@ namespace SamenHost
 
             Logging.Log("Startup", $"Loading Network...", LogType.INFO);
             Server.Start(4041);
+            ServerCLI.Begin();
 
             Logging.Log("Samen", $"Startup completed in {Math.Ceiling(DateTime.Now.Subtract(loadStartTime).TotalMilliseconds)}ms!", LogType.IMPORTANT);
-
-            Console.CancelKeyPress += (sender, e) =>
-            {
-                Console.Clear();
-                Console.WriteLine("!!! Stopping Samen will remove all session data !!!");
-                Console.WriteLine("!!! MAKE SURE YOU HAVE YOUR SCENES EXPORTED !!!");
-
-                Console.WriteLine("");
-                Console.WriteLine("Are you sure? Y/N (N)");
-
-                var key = Console.ReadKey();
-                if (key.Key != ConsoleKey.Y)
-                {
-                    e.Cancel = true;
-                    Console.Clear();
-                    Logging.Log("CLOSE", "Prevented shutdown", LogType.IMPORTANT);
-                }
-                else
-                {
-                    Console.Clear();
-                    Logging.Log("CLOSE", "Goodbye!", LogType.IMPORTANT);
-                    Environment.Exit(-1);
-                }
-            };
 
             while (true)
             {
